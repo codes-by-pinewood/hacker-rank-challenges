d3307474e8551f83c4fca426acb53c7896827b8c
codes-by-pinewood <52709218+codes-by-pinewood@users.noreply.github.com>
2022-06-24 12:21:04 +0200

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,18 @@
+//prints "hello world"
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%s", &s);
+    printf("Hello, World! ");
+    printf("%s", s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
